28865c762b3695755ed69b3b0986a9fb7f5c17b1
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-03-10 17:01:38 +0800

    register, add user, get users interfaces

---
+++ app/src/main/java/com/dglproject/partner/core/register/RegisterContract.java
@@ -0,0 +1,31 @@
+package com.dglproject.partner.core.register;
+
+import android.app.Activity;
+
+import com.google.firebase.auth.FirebaseUser;
+
+/**
+ * Created by Tortuvshin Byambaa on 3/10/2017.
+ */
+
+public interface RegisterContract {
+    interface View {
+        void onRegistrationSuccess(FirebaseUser firebaseUser);
+
+        void onRegistrationFailure(String message);
+    }
+
+    interface Presenter {
+        void register(Activity activity, String email, String password);
+    }
+
+    interface Interactor {
+        void performFirebaseRegistration(Activity activity, String email, String password);
+    }
+
+    interface OnRegistrationListener {
+        void onSuccess(FirebaseUser firebaseUser);
+
+        void onFailure(String message);
+    }
+}
---
+++ app/src/main/java/com/dglproject/partner/core/users/add/AddUserContract.java
@@ -0,0 +1,31 @@
+package com.dglproject.partner.core.users.add;
+
+import android.content.Context;
+
+import com.google.firebase.auth.FirebaseUser;
+
+/**
+ * Created by Tortuvshin Byambaa on 3/10/2017.
+ */
+
+public interface AddUserContract {
+    interface View {
+        void onAddUserSuccess(String message);
+
+        void onAddUserFailure(String message);
+    }
+
+    interface Presenter {
+        void addUser(Context context, FirebaseUser firebaseUser);
+    }
+
+    interface Interactor {
+        void addUserToDatabase(Context context, FirebaseUser firebaseUser);
+    }
+
+    interface OnUserDatabaseListener {
+        void onSuccess(String message);
+
+        void onFailure(String message);
+    }
+}
