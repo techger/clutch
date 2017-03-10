02b1b3515ef65df92268272c92915c66d4f76d6f
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-03-10 16:44:30 +0800

    constant utils

---
+++ app/src/main/java/com/dglproject/partner/utils/Constants.java
@@ -0,0 +1,14 @@
+package com.dglproject.partner.utils;
+
+/**
+ * Created by Tortuvshin Byambaa on 3/10/2017.
+ */
+public class Constants {
+    public static final String ARG_USERS = "users";
+    public static final String ARG_RECEIVER = "receiver";
+    public static final String ARG_RECEIVER_UID = "receiver_uid";
+    public static final String ARG_CHAT_ROOMS = "chat_rooms";
+    public static final String ARG_FIREBASE_TOKEN = "firebaseToken";
+    public static final String ARG_FRIENDS = "friends";
+    public static final String ARG_UID = "uid";
+}
