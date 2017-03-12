2ddac5916806e2cb581b9bb12c349b4e2eaf9fe1
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-03-12 20:23:21 +0800

    login interactor, presenter

---
+++ app/src/main/java/com/dglproject/partner/core/login/LoginInteractor.java
@@ -0,0 +1,55 @@
+package com.dglproject.partner.core.login;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.dglproject.partner.utils.Constants;
+import com.dglproject.partner.utils.SharedPrefUtil;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+/**
+ * Created by Tortuvshin Byambaa on 3/12/2017.
+ */
+public class LoginInteractor implements LoginContract.Interactor {
+    private LoginContract.OnLoginListener mOnLoginListener;
+
+    public LoginInteractor(LoginContract.OnLoginListener onLoginListener) {
+        this.mOnLoginListener = onLoginListener;
+    }
+
+    @Override
+    public void performFirebaseLogin(final Activity activity, final String email, String password) {
+        FirebaseAuth.getInstance()
+                .signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(activity, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (task.isSuccessful()) {
+                            mOnLoginListener.onSuccess(task.getResult().toString());
+                            updateFirebaseToken(task.getResult().getUser().getUid(),
+                                    new SharedPrefUtil(activity.getApplicationContext()).getString(Constants.ARG_FIREBASE_TOKEN, null));
+                        } else {
+                            mOnLoginListener.onFailure(task.getException().getMessage());
+                        }
+                    }
+                });
+    }
+
+    private void updateFirebaseToken(String uid, String token) {
+        FirebaseDatabase.getInstance()
+                .getReference()
+                .child(Constants.ARG_USERS)
+                .child(uid)
+                .child(Constants.ARG_FIREBASE_TOKEN)
+                .setValue(token);
+    }
+}
