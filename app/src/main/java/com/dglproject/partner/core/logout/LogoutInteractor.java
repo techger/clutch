4125c7e2822b8534ab741edc21ffccf45f692913
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-03-12 20:24:58 +0800

    logout interactor, presenter

---
+++ app/src/main/java/com/dglproject/partner/core/logout/LogoutInteractor.java
@@ -0,0 +1,24 @@
+package com.dglproject.partner.core.logout;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+/**
+ * Created by Tortuvshin Byambaa on 3/12/2017.
+ */
+public class LogoutInteractor implements LogoutContract.Interactor {
+    private LogoutContract.OnLogoutListener mOnLogoutListener;
+
+    public LogoutInteractor(LogoutContract.OnLogoutListener onLogoutListener) {
+        mOnLogoutListener = onLogoutListener;
+    }
+
+    @Override
+    public void performFirebaseLogout() {
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            FirebaseAuth.getInstance().signOut();
+            mOnLogoutListener.onSuccess("Successfully logged out!");
+        } else {
+            mOnLogoutListener.onFailure("No user logged in yet!");
+        }
+    }
+}
---
+++ app/src/main/java/com/dglproject/partner/core/logout/LogoutPresenter.java
@@ -0,0 +1,29 @@
+package com.dglproject.partner.core.logout;
+
+/**
+ * Created by Tortuvshin Byambaa on 3/12/2017.
+ */
+public class LogoutPresenter implements LogoutContract.Presenter, LogoutContract.OnLogoutListener {
+    private LogoutContract.View mLogoutView;
+    private LogoutInteractor mLogoutInteractor;
+
+    public LogoutPresenter(LogoutContract.View logoutView) {
+        mLogoutView = logoutView;
+        mLogoutInteractor = new LogoutInteractor(this);
+    }
+
+    @Override
+    public void logout() {
+        mLogoutInteractor.performFirebaseLogout();
+    }
+
+    @Override
+    public void onSuccess(String message) {
+        mLogoutView.onLogoutSuccess(message);
+    }
+
+    @Override
+    public void onFailure(String message) {
+        mLogoutView.onLogoutFailure(message);
+    }
+}
