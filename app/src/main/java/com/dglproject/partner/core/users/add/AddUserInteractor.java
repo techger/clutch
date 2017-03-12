906f0eece2b450e2bb503fde86069951db9456f1
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-03-12 20:28:43 +0800

    add user  interactor, presenter

---
+++ app/src/main/java/com/dglproject/partner/core/users/add/AddUserInteractor.java
@@ -0,0 +1,46 @@
+package com.dglproject.partner.core.users.add;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.dglproject.partner.R;
+import com.dglproject.partner.models.User;
+import com.dglproject.partner.utils.Constants;
+import com.dglproject.partner.utils.SharedPrefUtil;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+/**
+ * Created by Tortuvshin Byambaa on 3/12/2017.
+ */
+public class AddUserInteractor implements AddUserContract.Interactor {
+    private AddUserContract.OnUserDatabaseListener mOnUserDatabaseListener;
+
+    public AddUserInteractor(AddUserContract.OnUserDatabaseListener onUserDatabaseListener) {
+        this.mOnUserDatabaseListener = onUserDatabaseListener;
+    }
+
+    @Override
+    public void addUserToDatabase(final Context context, FirebaseUser firebaseUser) {
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+        User user = new User(firebaseUser.getUid(),
+                firebaseUser.getEmail(),
+                new SharedPrefUtil(context).getString(Constants.ARG_FIREBASE_TOKEN));
+        database.child(Constants.ARG_USERS)
+                .child(firebaseUser.getUid())
+                .setValue(user)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            mOnUserDatabaseListener.onSuccess(context.getString(R.string.user_successfully_added));
+                        } else {
+                            mOnUserDatabaseListener.onFailure(context.getString(R.string.user_unable_to_add));
+                        }
+                    }
+                });
+    }
+}
---
+++ app/src/main/java/com/dglproject/partner/core/users/add/AddUserPresenter.java
@@ -0,0 +1,33 @@
+package com.dglproject.partner.core.users.add;
+
+import android.content.Context;
+
+import com.google.firebase.auth.FirebaseUser;
+
+/**
+ * Created by Tortuvshin Byambaa on 3/12/2017.
+ */
+public class AddUserPresenter implements AddUserContract.Presenter, AddUserContract.OnUserDatabaseListener {
+    private AddUserContract.View mView;
+    private AddUserInteractor mAddUserInteractor;
+
+    public AddUserPresenter(AddUserContract.View view) {
+        this.mView = view;
+        mAddUserInteractor = new AddUserInteractor(this);
+    }
+
+    @Override
+    public void addUser(Context context, FirebaseUser firebaseUser) {
+        mAddUserInteractor.addUserToDatabase(context, firebaseUser);
+    }
+
+    @Override
+    public void onSuccess(String message) {
+        mView.onAddUserSuccess(message);
+    }
+
+    @Override
+    public void onFailure(String message) {
+        mView.onAddUserFailure(message);
+    }
+}
