20b03ac0e44c6a5ade936522c9c2c78866f8f8eb
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-03-12 20:07:35 +0800

    firebase instance id service, messaging service

---
+++ app/src/main/java/com/dglproject/partner/fcm/MyFirebaseMessagingService.java
@@ -0,0 +1,98 @@
+package com.dglproject.partner.fcm;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
+
+import com.dglproject.partner.Main;
+import com.dglproject.partner.R;
+import com.dglproject.partner.events.PushNotificationEvent;
+import com.dglproject.partner.ui.activities.ChatActivity;
+import com.dglproject.partner.utils.Constants;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import org.greenrobot.eventbus.EventBus;
+
+/**
+ * Created by Tortuvshin Byambaa on 3/19/2017.
+ */
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "MyFirebaseMsgService";
+
+    /**
+     * Called when message is received.
+     *
+     * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
+     */
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+
+            String title = remoteMessage.getData().get("title");
+            String message = remoteMessage.getData().get("text");
+            String username = remoteMessage.getData().get("username");
+            String uid = remoteMessage.getData().get("uid");
+            String fcmToken = remoteMessage.getData().get("fcm_token");
+
+            // Don't show notification if chat activity is open.
+            if (!Main.isChatActivityOpen()) {
+                sendNotification(title,
+                        message,
+                        username,
+                        uid,
+                        fcmToken);
+            } else {
+                EventBus.getDefault().post(new PushNotificationEvent(title,
+                        message,
+                        username,
+                        uid,
+                        fcmToken));
+            }
+        }
+    }
+
+    /**
+     * Create and show a simple notification containing the received FCM message.
+     */
+    private void sendNotification(String title,
+                                  String message,
+                                  String receiver,
+                                  String receiverUid,
+                                  String firebaseToken) {
+        Intent intent = new Intent(this, ChatActivity.class);
+        intent.putExtra(Constants.ARG_RECEIVER, receiver);
+        intent.putExtra(Constants.ARG_RECEIVER_UID, receiverUid);
+        intent.putExtra(Constants.ARG_FIREBASE_TOKEN, firebaseToken);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_messaging)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+}
