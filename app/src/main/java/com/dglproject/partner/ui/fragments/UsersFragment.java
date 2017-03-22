09a907c5209e53b8c2b88cb2661186a2c80bdb82
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-03-22 20:42:19 +0800

    chat activity fragment some methods

---
+++ app/src/main/java/com/dglproject/partner/ui/fragments/UsersFragment.java
@@ -0,0 +1,143 @@
+package com.dglproject.partner.ui.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.dglproject.partner.R;
+import com.dglproject.partner.core.users.get.all.GetUsersContract;
+import com.dglproject.partner.core.users.get.all.GetUsersPresenter;
+import com.dglproject.partner.models.User;
+import com.dglproject.partner.ui.activities.ChatActivity;
+import com.dglproject.partner.ui.adapters.UserListingRecyclerAdapter;
+import com.dglproject.partner.utils.ItemClickSupport;
+
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link UsersFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link UsersFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class UsersFragment extends Fragment implements GetUsersContract.View, ItemClickSupport.OnItemClickListener, SwipeRefreshLayout.OnRefreshListener {
+    public static final String ARG_TYPE = "type";
+    public static final String TYPE_CHATS = "type_chats";
+    public static final String TYPE_ALL = "type_all";
+
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+    private RecyclerView mRecyclerViewAllUserListing;
+
+    private UserListingRecyclerAdapter mUserListingRecyclerAdapter;
+
+    private GetUsersPresenter mGetUsersPresenter;
+
+    public static UsersFragment newInstance(String type) {
+        Bundle args = new Bundle();
+        args.putString(ARG_TYPE, type);
+        UsersFragment fragment = new UsersFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View fragmentView = inflater.inflate(R.layout.fragment_users, container, false);
+        bindViews(fragmentView);
+        return fragmentView;
+    }
+
+    private void bindViews(View view) {
+        mSwipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipe_refresh_layout);
+        mRecyclerViewAllUserListing = (RecyclerView) view.findViewById(R.id.recycler_view_all_user_listing);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        init();
+    }
+
+    private void init() {
+        mGetUsersPresenter = new GetUsersPresenter(this);
+        getUsers();
+        mSwipeRefreshLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                mSwipeRefreshLayout.setRefreshing(true);
+            }
+        });
+
+        ItemClickSupport.addTo(mRecyclerViewAllUserListing)
+                .setOnItemClickListener(this);
+
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+    }
+
+    @Override
+    public void onRefresh() {
+        getUsers();
+    }
+
+    private void getUsers() {
+        if (TextUtils.equals(getArguments().getString(ARG_TYPE), TYPE_CHATS)) {
+
+        } else if (TextUtils.equals(getArguments().getString(ARG_TYPE), TYPE_ALL)) {
+            mGetUsersPresenter.getAllUsers();
+        }
+    }
+
+    @Override
+    public void onItemClicked(RecyclerView recyclerView, int position, View v) {
+        ChatActivity.startActivity(getActivity(),
+                mUserListingRecyclerAdapter.getUser(position).email,
+                mUserListingRecyclerAdapter.getUser(position).uid,
+                mUserListingRecyclerAdapter.getUser(position).firebaseToken);
+    }
+
+    @Override
+    public void onGetAllUsersSuccess(List<User> users) {
+        mSwipeRefreshLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                mSwipeRefreshLayout.setRefreshing(false);
+            }
+        });
+        mUserListingRecyclerAdapter = new UserListingRecyclerAdapter(users);
+        mRecyclerViewAllUserListing.setAdapter(mUserListingRecyclerAdapter);
+        mUserListingRecyclerAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onGetAllUsersFailure(String message) {
+        mSwipeRefreshLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                mSwipeRefreshLayout.setRefreshing(false);
+            }
+        });
+        Toast.makeText(getActivity(), "Error: " + message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onGetChatUsersSuccess(List<User> users) {
+
+    }
+
+    @Override
+    public void onGetChatUsersFailure(String message) {
+
+    }
+}
