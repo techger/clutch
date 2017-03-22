1612354cfdb4dbdb9aa652ff2a52d62dcea6b71c
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-03-22 20:37:37 +0800

    created user listing adapter

---
+++ app/src/main/java/com/dglproject/partner/ui/adapters/UserListingPagerAdapter.java
@@ -0,0 +1,36 @@
+package com.dglproject.partner.ui.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.dglproject.partner.ui.fragments.UsersFragment;
+
+/**
+ * Created by Tortuvshin Byambaa on 3/22/2017.
+ */
+public class UserListingPagerAdapter extends FragmentPagerAdapter {
+    private static final Fragment[] sFragments = new Fragment[]{/*UsersFragment.newInstance(UsersFragment.TYPE_CHATS),*/
+            UsersFragment.newInstance(UsersFragment.TYPE_ALL)};
+    private static final String[] sTitles = new String[]{/*"Chats",*/
+            "All Users"};
+
+    public UserListingPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return sFragments[position];
+    }
+
+    @Override
+    public int getCount() {
+        return sFragments.length;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return sTitles[position];
+    }
+}
